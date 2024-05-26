b963bf22d402e08dab0d303b1b038e7596ea00d8
antretrou <156452783+antretrou@users.noreply.github.com>
2024-05-26 16:35:50 +0700

    update security

--- BackEnd/src/main/java/com/example/salesystematthestore/utils/JwtTokenHelper.java
+++ BackEnd/src/main/java/com/example/salesystematthestore/utils/JwtTokenHelper.java
@@ -15,36 +15,36 @@ public class JwtTokenHelper {
     @Value("${jwt.secretkey}")
     private String key;
 
-    public String generateToken(Users users){
+    public String generateToken(Users users) {
         SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
         String token = Jwts.builder()
                 .setIssuer("ATDStore")
                 .setSubject("JWT Token")
-                .claim("username",users.getUsername())
-                //.claim("role", users.getRoles().getRoleName())
-               // .claim("name", users.getFullname())
+                .claim("username", users.getUsername())
+                .claim("role", users.getRole().getName())
+                .claim("name", users.getFullName())
                 .claim("address", users.getAddress())
                 .claim("id", users.getId())
+                .claim("couterAddress",users.getCounter().getAddress())
                 .claim("phoneNumber", users.getPhoneNumber())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date().getTime())+30000000))
+                .setExpiration(new Date((new Date().getTime()) + 30000000))
                 .signWith(secretKey).compact();
         return token;
     }
 
-    public boolean verifyToken(String token){
+    public boolean verifyToken(String token) {
         SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(key));
-        try{
+        try {
             Jwts.parser()
                     .verifyWith(secretKey)
                     .build()
                     .parseSignedClaims(token);
             return true;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
     }
-
 
 
 }
