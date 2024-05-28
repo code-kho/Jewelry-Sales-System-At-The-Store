d84fad16c1760d230efc3463a1c67fe4d30a7607
antretrou <156452783+antretrou@users.noreply.github.com>
2024-05-29 01:58:32 +0700

    update api

--- BackEnd/src/main/java/com/example/salesystematthestore/repository/ProductRepository.java
+++ BackEnd/src/main/java/com/example/salesystematthestore/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.example.salesystematthestore.repository;
 
 import com.example.salesystematthestore.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,10 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Page<Product> findByProductNameContainsAndProductCounterList_Counter_Id(String productName, int id, Pageable pageable);
+
+
+    Page<Product> findByProductCounterList_Counter_Id(int id, Pageable pageable);
+
 
 }
