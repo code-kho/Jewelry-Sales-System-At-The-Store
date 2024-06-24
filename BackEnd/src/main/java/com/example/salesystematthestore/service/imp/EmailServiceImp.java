77545404b43d49344b0dc51768e6e4ef1559bb69
antretrou <156452783+antretrou@users.noreply.github.com>
2024-06-24 13:37:29 +0700

    update new all

--- BackEnd/src/main/java/com/example/salesystematthestore/service/imp/EmailServiceImp.java
+++
@@ -1,6 +0,0 @@
-package com.example.salesystematthestore.service.imp;
-
-public interface EmailServiceImp {
-
-    public void sendEmail(String from, String to, String subject, String text);
-}
--- BackEnd/src/main/java/com/example/salesystematthestore/service/imp/LoginServiceImp.java
+++
@@ -1,7 +0,0 @@
-package com.example.salesystematthestore.service.imp;
-
-public interface LoginServiceImp {
-
-    boolean checkLogin(String username,String password);
-
-}
